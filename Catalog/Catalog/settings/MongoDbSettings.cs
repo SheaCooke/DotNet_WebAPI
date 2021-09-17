37a651eb4dd81ff8927bdab96d431b3ff23d7d8c
shea cooke <SheaSCooke@gmail.com>
2021-09-17 15:03:31 -0700

    got authentication working with secret store

--- Catalog/Catalog/settings/MongoDbSettings.cs
+++ Catalog/Catalog/settings/MongoDbSettings.cs
@@ -10,11 +10,13 @@ namespace Catalog.settings
         //declare settings from appsettings.json as properties 
         public string Host { get; set; }
         public int Port { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
 
         public string ConnectionString 
         {get
             {
-                return $"mongodb://{Host}:{Port}";
+                return $"mongodb://{User}:{Password}@{Host}:{Port}";
             }
         
         }
