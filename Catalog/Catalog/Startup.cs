19ab7f2bb691c95d869d4f0b4772fad65baf638e
shea cooke <SheaSCooke@gmail.com>
2021-09-22 15:39:40 -0700

    added configurations for docker

--- Catalog/Catalog/Startup.cs
+++ Catalog/Catalog/Startup.cs
@@ -74,7 +74,11 @@ namespace Catalog
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog v1"));
             }
 
-            app.UseHttpsRedirection();
+            if (env.IsDevelopment())
+            {
+                app.UseHttpsRedirection();
+            }
+            
 
             app.UseRouting();
 
@@ -104,7 +108,7 @@ namespace Catalog
                                 )
 
                             }
-
+                             
                             );
 
                         context.Response.ContentType = MediaTypeNames.Application.Json;
